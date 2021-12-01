242e8442adb49f80110fa7698d0aa74a7ebce106
Tomás Marques <tomasm576@gmail.com>
2021-12-01 04:25:01 +0000

    sumByMonthYearChart

--- src/components/statistics/Statistics.vue
+++ src/components/statistics/Statistics.vue
@@ -1,43 +1,89 @@
 <template>
   <h3 class="mt-5 mb-3">Statistics</h3>
+  <SumYearChart
+    style="width:50%; height 50%"
+    v-if="date && value"
+    v-bind:labels="date"
+    v-bind:data="value"
+  />
 </template>
 
 <script>
+import { defineComponent } from "vue"
+import SumYearChart from "./SumYearChart.vue"
 
-
-export default {
-  name: 'Statistics',
+export default defineComponent({
+  name: "Statistics",
   components: {
+    SumYearChart,
   },
-  data () {
+  data() {
     return {
-        transactions: []
+      transactions: [],
+      date: [],
+      value: [],
+      merged: [],
+      resultobj: [],
     }
   },
-  computed: {
-  },
   methods: {
-    loadTransactions () {
-      this.$axios.get('transactions')
+    loadTransactions() {
+      this.$axios
+        .get("transactions")
         .then((response) => {
           this.transactions = response.data.data
-          if(this.$store.state.user.user_type == "V"){
-              this.transactions = this.transactions.filter(transaction => transaction.vcard == this.$store.state.user.username)
+          if (this.$store.state.user.user_type == "V") {
+            this.transactions = this.transactions.filter(
+              (transaction) =>
+                transaction.vcard == this.$store.state.user.username
+            )
           }
-          console.log(this.transactions)
+
+          this.date = this.transactions.map((transaction) =>
+            transaction.date.slice(0, 7)
+          )
+          this.value = this.transactions.map((transaction) => transaction.value)
+
+          //https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
+
+          for (let i = 0; i < Object.keys(this.transactions).length; i++) {
+            this.merged[i] = {
+              date: this.date[i],
+              value: this.value[i],
+            }
+          }
+
+          var result = []
+          var datenew = []
+          var valuenew = []
+          this.merged.reduce(function (res, mergedArray) {
+            if (!res[mergedArray.date]) {
+              res[mergedArray.date] = { date: mergedArray.date, value: 0 }
+              result.push(res[mergedArray.date])
+            }
+            res[mergedArray.date].value += parseFloat(mergedArray.value)
+            return res
+          }, {})
+
+          result.forEach(function (item) {
+            valuenew.push(item.value)
+            datenew.push(item.date)
+          })
+
+          this.date = datenew
+          this.value = valuenew
         })
         .catch((error) => {
           console.log(error)
         })
     },
-   
   },
-  mounted () {
+
+  mounted() {
     this.loadTransactions()
-  }
-}
+  },
+})
 </script>
 
 <style scoped>
-
 </style>
